ecccf854eda9623ca37e914cfe430321c6ece4de
Maria Cabrera <mariacabrera03@gmail.com>
2025-09-29 14:25:20 -0300

    Eliminacion archivo vscode

--- TP2/js/carousel.js
+++ TP2/js/carousel.js
@@ -1,27 +1,27 @@
-const url = "https://vj.interfaces.jima.com.ar/api/v2";
+// const url = "https://vj.interfaces.jima.com.ar/api/v2";
 
-let carrusel = document.querySelector("#carousel-container");
+// let carrusel = document.querySelector("#carousel-container");
 
 
-fetch(url)
-    .then(response => response.json())
-    .then(games => {
-        console.log('Juegos disponibles:', games);
-        // Procesar la lista de juegos con campos adicionales
-        games.forEach(game => {
-            console.log(`${game.name} - Rating: ${game.rating}`);
-            console.log(`Descripción: ${game.description}`);
-            console.log(`Imagen optimizada: ${game.background_image_low_res}`);
-        });
-    })
-    .catch(error => {
-        console.error('Error al obtener los juegos:', error);
-    });
+// fetch(url)
+//     .then(response => response.json())
+//     .then(games => {
+//         console.log('Juegos disponibles:', games);
+//         // Procesar la lista de juegos con campos adicionales
+//         games.forEach(game => {
+//             console.log(`${game.name} - Rating: ${game.rating}`);
+//             console.log(`Descripción: ${game.description}`);
+//             console.log(`Imagen optimizada: ${game.background_image_low_res}`);
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Error al obtener los juegos:', error);
+//     });
 
-    async function cargarJuegos() {
-        carrusel.innerHTML =
-         `<div class="carousel">
+//     async function cargarJuegos() {
+//         carrusel.innerHTML =
+//          `<div class="carousel">
          
-        `
+//         `
         
-    }
+//     }
